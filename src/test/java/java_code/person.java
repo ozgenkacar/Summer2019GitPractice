b644a52ad0f5412f8c78f1963698fdc6eb933d64
ozgenkacar <ozkcr@icloud.com>
2019-12-05 11:21:00 -0800

    first commit

---
+++ src/test/java/java_code/person.java
@@ -0,0 +1,9 @@
+package java_code;
+
+public class person {
+    private int age;
+
+    public person(int age) {
+        this.age=age;
+    }
+}
---
+++ src/test/java/java_code/student.java
@@ -0,0 +1,10 @@
+package java_code;
+
+public class student extends person{
+
+    public student (int age) {
+        super(age);
+    }
+
+
+}
